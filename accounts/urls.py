326a9a7919f65e7ba7bf009491feba349879854e
zimacl <zimacl@zima.zima.zima>
2021-02-28 05:55:09 +0200

    delete view added...

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import login_view, logout_view, register_view, update_view
+from .views import (
+    login_view, logout_view, register_view, update_view, delete_view)
 
 app_name = 'accounts'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('register_done/', login_view, name='register_done'),
     path('update/', update_view, name='update'),
+    path('delete/', delete_view, name='delete'),
 ]
