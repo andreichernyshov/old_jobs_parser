8cb04d3e7fd256f4c15312d31ebb4f864ef5f177
zimacl <zimacl@zima.zima.zima>
2021-03-01 20:37:13 +0200

    emails through gooogle for a while - Done

--- send_emails.py
+++ send_emails.py
@@ -12,6 +12,13 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "scraping.settings"
 django.setup()
 from scrap.models import Vacancy
 
+from scraping.settings import EMAIL_HOST_USER
+
+
+subject = 'Разсылка вакансий'
+text_content = 'Разсылка'
+from_email = EMAIL_HOST_USER
+empty = '<h2>По запросу ничего не найденно.</h2>'
 
 User = get_user_model()
 qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
@@ -25,12 +32,21 @@ if users_dct:
     for pair in users_dct.keys():
         params['city_id__in'].append(pair[0])
         params['language_id__in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params)[:10]
-
-
-subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
-text_content = 'This is an important message.'
-html_content = '<p>This is an <strong>important</strong> message.</p>'
-msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-msg.attach_alternative(html_content, "text/html")
-msg.send()
+    qs = Vacancy.objects.filter(**params).values()[:3]
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dct.items():
+        rows = vacancies.get(keys, [])
+        html = ''
+        for row in rows:
+            html += f'<h5><a href="{ row["url"] }">{ row["title"] }</a></h5>'
+            html += f'<p> { row["description"] } </p>'
+            html += f'<p> { row["company"] } </p><br><hr>'
+        _html = html if html else empty
+        for email in emails:
+            to = email
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(_html, "text/html")
+            msg.send()
